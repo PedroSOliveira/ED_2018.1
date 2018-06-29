fe6da64d2e7412ef919d89ce0a77f21668e80b5b
PedroSOliveira <37816505+PedroSOliveira@users.noreply.github.com>
2018-06-29 19:09:57 -0300

    Update main.cpp

--- 03_Vector/main.cpp
+++ 03_Vector/main.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <vector>
 
@@ -16,17 +17,18 @@ struct Vetor{
         this->_data = new int[capacidade];//retorno zerado
     }
     void push_back(int value){
-        if(this->_size == this->_capacidade)
-            return;
-            //this->reserve(2 * _capacitade);
-        this->_data[this->_size] = value;
+        if(this->_size == this->_capacidade)//Verifica se o vetor nao ta lotado. se tiver chama reserve
+        this->reserve(2 * _capacidade);
+        this->_data[this->_size] = value;// adiciona na uultima posicao e incrementa size
         this->_size += 1;
     }
     void pop_back(){
         if(this->_size == 0)
             return;
-        this->_data[_size] = 0;
-        this->_size -= 1;
+        this->_data[this->_size] = 0;
+        this->_size -=1;
+
+
     }
 
     //retornar a refencia à variavel dessa posicao
@@ -43,7 +45,10 @@ struct Vetor{
         return &this->_data[0];
     }
     int * end(){
-        return &this->_data[this->_size];
+        if(this->_size == 0){
+            return nullptr;
+        }
+        return &this->_data[this->_size];//Retorna valor da ultima posição
     }
     int size(){
         return this->_size;
@@ -55,16 +60,20 @@ struct Vetor{
     }
 
     void reserve(int new_capacity){
-        if(this->_capacidade < new_capacity)
-            return;
-        int *novos_dados = new int[this->_size * 2];
-        for(int i = 0; i < this->_size; i++){
-            novos_dados[i] = this->_data[i];
+        if(this->_capacidade == new_capacity)
+         return;
+        if(this->_capacidade > new_capacity){
+            if(this->_size > new_capacity){
+                this->_size =  new_capacity;
+            }
         }
+     int *novos_dados = new int[new_capacity];//criando ponteiro com tamannho
+     for(int i = 0; i < this->_size; i++){    //da nova capacidade
+         novos_dados[i] = this->_data[i]; // novos dados recebendo os antigos tbm
+     }
+     this->_data = novos_dados; // passar novos dados para data
+     this->_capacidade = _size; //atualiza a capacidade
 
-        this->_data = novos_dados;
-        //salva o bloco antigo em algum lugar
-        //pede um novo bloco de dados
     }
 
 };
@@ -72,7 +81,7 @@ struct Vetor{
 
 int main(){
 
-    Vetor teste_vetor(5);
+    Vetor teste_vetor(4);
 
     teste_vetor.push_back(1);
     teste_vetor.push_back(2);
@@ -97,9 +106,19 @@ int main(){
     }
 
     cout << endl;
-    teste_vetor.reserve(30);
+
+    teste_vetor.reserve(3);
+    teste_vetor.push_back(99);
+
+    for(auto i : teste_vetor){
+        cout << i << " ";
+    }
 
     cout << "Capacidade nova: " << teste_vetor.capacity() << endl;
+
+    for(auto i : teste_vetor){
+        cout << i << " ";
+    }
 
     return 0;
     }
