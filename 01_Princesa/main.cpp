37bf29b46bf0e160ec1e72e80b6e824d51c14f54
PedroSOliveira <37816505+PedroSOliveira@users.noreply.github.com>
2018-06-29 19:06:07 -0300

    Update main.cpp

--- 01_Princesa/main.cpp
+++ 01_Princesa/main.cpp
@@ -4,45 +4,40 @@
 using namespace std;
 
 int main(){
+    int cavalheiros_total, cavalheiro_inicial, salto;
+    cout << "Digite o numero de cavalheiros: "  ;
+    cin >> cavalheiros_total ;
 
-int cavalheiros_total, cavalheiro_inicial;
- cout << "Digite o numero de cavalheiros: "  ;
- cin >> cavalheiros_total ;
+    int vetor[cavalheiros_total];
 
-int vetor[cavalheiros_total];
+    cout << "Escolha um cavalheiro para começar: "  ;
+    cin >> cavalheiro_inicial ;
+    cout << "Escolha o número de saltos: "  ;
+    cin >> salto ;
 
-for(int i = 0; i < cavalheiros_total; i++){
-    vetor[i] = i + 1;
-}
-int j;
+    //preenchendo todos com 1
+    int i, cont = 0;
+    for(i = 0; i < cavalheiros_total; i++){
+           vetor[i] = 1;
+    }
 
-cout << "Cavalheiros: " << endl;
-for(j = 0; j < cavalheiros_total; j++){
-    printf("%d""\n", vetor[j]);
-
-}
-
-int cavalheiro_vencedor;
-int total = cavalheiros_total;
-
-
-cout << "Escolha um cavalheiro pra inicia r: ";
-cin >> cavalheiro_inicial ;
-
-    for(int a = 0; a < cavalheiros_total; a++){
-            if( cavalheiro_inicial == vetor[a]){
-                do{
-                    vetor[(a + 1) % cavalheiros_total] = 0;
-                    cavalheiro_inicial = vetor[(a+2) % cavalheiros_total ];
-                    total--;
-                }while(total > 1);
+    while(cont < cavalheiros_total -1){
+        int ant = cavalheiro_inicial%cavalheiros_total;
+        i = 0;
+        while(i < salto){
+            if(vetor[ (cavalheiro_inicial+1)%cavalheiros_total ] != 0 && (cavalheiro_inicial+1)%cavalheiros_total != ant){
+                i++;
             }
+            cavalheiro_inicial++;
         }
-
-
-cavalheiro_vencedor = cavalheiro_inicial;
-cout << "O cavalheiro que conquistou a princesa foi: ";
-cout <<  cavalheiro_vencedor  << endl;
+        vetor[cavalheiro_inicial%cavalheiros_total] = 0;
+        while(vetor[cavalheiro_inicial%cavalheiros_total] == 0){
+            cavalheiro_inicial++;
+        }
+        cont++;
+    }
+    cout <<" O cavalheiro que conquistou a princesa foi : ";
+    cout << cavalheiro_inicial%cavalheiros_total << endl;
 
     return 0;
 }
