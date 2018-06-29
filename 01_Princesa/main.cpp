00f9d7c14198fc8ec3acb04de591e9c26c70632c
PedroSOliveira <37816505+PedroSOliveira@users.noreply.github.com>
2018-06-29 19:20:39 -0300

    Update main.cpp

--- 01_Princesa/main.cpp
+++ 01_Princesa/main.cpp
@@ -7,20 +7,20 @@ int main(){
     int cavalheiros_total, cavalheiro_inicial, salto;
     cout << "Digite o numero de cavalheiros: "  ;
     cin >> cavalheiros_total ;
-
+    
     int vetor[cavalheiros_total];
-
+    
     cout << "Escolha um cavalheiro para começar: "  ;
     cin >> cavalheiro_inicial ;
     cout << "Escolha o número de saltos: "  ;
     cin >> salto ;
-
+    
     //preenchendo todos com 1
     int i, cont = 0;
     for(i = 0; i < cavalheiros_total; i++){
-           vetor[i] = 1;
+        vetor[i] = 1;
     }
-
+    
     while(cont < cavalheiros_total -1){
         int ant = cavalheiro_inicial%cavalheiros_total;
         i = 0;
@@ -38,6 +38,6 @@ int main(){
     }
     cout <<" O cavalheiro que conquistou a princesa foi : ";
     cout << cavalheiro_inicial%cavalheiros_total << endl;
-
+    
     return 0;
 }
